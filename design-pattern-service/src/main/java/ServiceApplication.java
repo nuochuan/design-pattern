8d74d227a5a48019f22dd1bec9567df9407148b1
nuochuan <68357790@qq.com>
2021-09-29 21:28:24 +0800

    工程初始化

--- design-pattern-service/src/main/java/ServiceApplication.java
+++ design-pattern-service/src/main/java/ServiceApplication.java
@@ -1,15 +1,9 @@
-import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.scheduling.annotation.EnableAsync;
+
 
 @SpringBootApplication
 public class ServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(OmsServiceApplication.class, args);
+        SpringApplication.run(ServiceApplication.class, args);
     }
 }
 
